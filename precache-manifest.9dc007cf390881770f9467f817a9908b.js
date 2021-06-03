b99f9219e453ed4c59e28602180ac6bc0c8663fe
Aliabbas Merchant <merchantaliabbas@gmail.com>
2021-06-03 15:31:41 +0530

    Updates

---
+++ precache-manifest.9dc007cf390881770f9467f817a9908b.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6614192c43c950e24d6c6d6755d3f582",
+    "url": "/BlockEye/index.html"
+  },
+  {
+    "revision": "dd03476d2308b4eb3629",
+    "url": "/BlockEye/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "25302b6b4a655557e170",
+    "url": "/BlockEye/static/css/main.b241398c.chunk.css"
+  },
+  {
+    "revision": "dd03476d2308b4eb3629",
+    "url": "/BlockEye/static/js/2.3e1dce76.chunk.js"
+  },
+  {
+    "revision": "25302b6b4a655557e170",
+    "url": "/BlockEye/static/js/main.ebe24ff3.chunk.js"
+  },
+  {
+    "revision": "7526c3d4319fecf7dd7f",
+    "url": "/BlockEye/static/js/runtime-main.aec7b7fd.js"
+  },
+  {
+    "revision": "ac1432a494446f561bb860821b48ccf6",
+    "url": "/BlockEye/static/media/AgriChain.ac1432a4.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/BlockEye/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "563336a614fb5c0ce4bfed9f44b86fd5",
+    "url": "/BlockEye/static/media/Blockeye-Cropped.563336a6.png"
+  },
+  {
+    "revision": "7fc9fbdc8eee6fe5d067e391fccb88c4",
+    "url": "/BlockEye/static/media/CT_img.7fc9fbdc.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/BlockEye/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/BlockEye/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/BlockEye/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/BlockEye/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/BlockEye/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/BlockEye/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/BlockEye/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8a3c03228c27141f4ae7cdc433033d25",
+    "url": "/BlockEye/static/media/ai-bct-2-min.8a3c0322.png"
+  },
+  {
+    "revision": "28b17b2acb99d5458ebd58ab1d551072",
+    "url": "/BlockEye/static/media/blockchain-min.28b17b2a.jpg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/BlockEye/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/BlockEye/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/BlockEye/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/BlockEye/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/BlockEye/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/BlockEye/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/BlockEye/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/BlockEye/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/BlockEye/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/BlockEye/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/BlockEye/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/BlockEye/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/BlockEye/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/BlockEye/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/BlockEye/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "4baca3e00e7f266a21426ab8146c4c1c",
+    "url": "/BlockEye/static/media/humanoid-min.4baca3e0.jpg"
+  },
+  {
+    "revision": "ac685d3638f498ba65a3ef6b6dee237f",
+    "url": "/BlockEye/static/media/temp.ac685d36.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21be1a29cb7b621c86bbcd9c1a37f842.js"
+  "/BlockEye/precache-manifest.9dc007cf390881770f9467f817a9908b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BlockEye/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
